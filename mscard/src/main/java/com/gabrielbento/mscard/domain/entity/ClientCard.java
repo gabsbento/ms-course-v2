a49b17dbad6bfbb3730bd51bf008e7cfd80de269
Gabriel Bento <gab.silva.bento@gmail.com>
2023-10-20 09:40:20 -0300

    created entity of client card

---
+++ mscard/src/main/java/com/gabrielbento/mscard/domain/entity/ClientCard.java
@@ -0,0 +1,19 @@
+package com.gabrielbento.mscard.domain.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "client_card")
+@NoArgsConstructor
+@Data
+public class ClientCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+    private BigDecimal limitValue;
+}
