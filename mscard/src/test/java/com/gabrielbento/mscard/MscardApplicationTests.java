343d3f9b50715b40254dcf1791ee3c6c7d34bb22
Gabriel Bento <gab.silva.bento@gmail.com>
2023-10-20 09:35:38 -0300

    initial commit

---
+++ mscard/src/test/java/com/gabrielbento/mscard/MscardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbento.mscard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
